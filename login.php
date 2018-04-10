74fc3a996411d48c95698b9c3a91931e2484bbf6
eswar557 <apreddyvce@gmail.com>
2018-04-10 03:42:08 -0400

    login

--- login.php
+++ login.php
@@ -13,30 +13,34 @@
     </head>
 
     <body>
-        <form method="POST" action="index.php">
-            <?php if(@$_GET['error'] == 1) { print $_SESSION['password']?>
-            <div class="alert alert-danger">
-                Login incorrect. Please try again
-            </div>
-            <?php } ?>
-            <div class="card">
-                <div class="card-head">Login</div>
-                <div class="card-body">
-                    <form class="form" autocomplete="off">
-                        <div class="form-group">
-                            <label for="email">Email address</label>
-                            <input type="text" class="form-control" name="email" id="email" placeholder="Enter email">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-sm-12 col-md-6">
+                    <form method="POST" action="index.php">
+                        <?php if(@$_GET['error'] == 1) { print $_SESSION['password']?>
+                        <div class="alert alert-danger">
+                            Login incorrect. Please try again
                         </div>
-                        <div class="form-group">
-                            <label for="password">Password</label>
-                            <input type="password" class="form-control" name="password" id="password" placeholder="Password">
+                        <?php } ?>
+                        <div class="card">
+                            <div class="card-head">Login</div>
+                            <div class="card-body">
+                                <div class="form-group">
+                                    <label for="email">Email address</label>
+                                    <input type="text" class="form-control" name="email" id="email" placeholder="Enter email">
+                                </div>
+                                <div class="form-group">
+                                    <label for="password">Password</label>
+                                    <input type="password" class="form-control" name="password" id="password" placeholder="Password">
+                                </div>
+                                <input type="submit" class="btn btn-primary" name="op" value="login" />
+                            </div>
                         </div>
-                        <input type="submit" class="btn btn-primary" name="op" value="login" />
+                    
                     </form>
                 </div>
             </div>
-
-        </form>
+        </div>
     </body>
 
 </html>
