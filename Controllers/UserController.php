7985147bb0ca12f7483da183b8e0e07cea013d0b
eswar557 <apreddyvce@gmail.com>
2018-04-10 02:57:51 -0400

    login

--- Controllers/UserController.php
+++ Controllers/UserController.php
@@ -1,4 +1,5 @@
 <?php
+include_once('../config.php')
 class UserController {
     function UserController()
     {
@@ -11,8 +12,6 @@ class UserController {
 
     function login($email, $password) {
         if($this->authenticate($email, $password)) {
-            session_start();
-            $_SESSION['user'] = $email;
             return true;
         } else {
             return false;
@@ -21,7 +20,13 @@ class UserController {
 
     function authenticate($email, $password) {
         $authentic = false;
-        if($email == 'admin' && $password == 'admin') {
+        $sql = "SELECT id FROM admin WHERE email = '$email' and password = 'md5($password)'";
+        $result = mysqli_query($db,$sql);
+        $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+        $count = mysqli_num_rows($result);
+        if($count == 1) {
+            session_start();
+            $_SESSION['user'] = $row['first_name'].' '.$row['last_name'];
             $authentic = true;
         }
         return $authentic;
---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+   define('DB_SERVER', 'localhost');
+   define('DB_USERNAME', 'root');
+   define('DB_PASSWORD', 'Param123');
+   define('DB_DATABASE', 'paramDB');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
