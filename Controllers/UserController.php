24accf6da76b2317eb42a791bd35a6e507a11b1d
eswar557 <apreddyvce@gmail.com>
2018-04-10 03:17:10 -0400

    login

--- Controllers/UserController.php
+++ Controllers/UserController.php
@@ -25,7 +25,7 @@ class UserController {
         $result = mysqli_query($db,$sql);
         $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
         $count = mysqli_num_rows($result);
-        if($count == 1) {
+        if($count > 0) {
             session_start();
             $_SESSION['user'] = $row['first_name'];//.' '.$row['last_name'];
             $authentic = true;
