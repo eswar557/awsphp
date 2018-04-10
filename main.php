d450c294d8325e68cd0658e5a3c171dcf5e9d2fd
eswar557 <apreddyvce@gmail.com>
2018-04-10 02:40:02 -0400

    login

--- main.php
+++ main.php
@@ -1,5 +1,5 @@
 <?php
-    require_once('../Models/UserModel.php');
+    require_once('./Models/UserModel.php');
     session_start();
     if(!isset($_SESSION['user'])) {
         header("Location:login.php");
